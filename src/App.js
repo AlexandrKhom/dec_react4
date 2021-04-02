060680e9afa5768ec105da13a7182298f65a2e92
AlexandrKhom <alex988andr@gmail.com>
2021-04-02 22:09:20 +0300

    less 2.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const Header = ({counter}) => {
+    return (<h2>
+        {counter}
+    </h2>)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setCounter] = useState(0);
+    // const [newCounter, setNewCounter] = useState(3);
+
+    const click = () => {
+        // let counterVel = counter + newCounter;
+        // setNewCounter(newCounter + 2);
+        // setCounter(counterVel);
+        setCounter(counter + 1);
+        console.log(counter);
+    }
+    const [hide, sethide] = useState(true)
+    const toggle = () => {
+        sethide(!hide)
+    }
+    const [todos, settodos] = useState([`hello`, `alex`]);
+    const change = () => {
+        const newArr = [...todos];
+        newArr[0] = Math.random();
+        settodos(newArr);
+    }
+
+    return (
+        <div className="App">
+
+            {hide && <Header counter={counter}/>}
+
+            <button onClick={click}>increment</button>
+            <br/>
+            <button onClick={toggle}>toggle</button>
+            <hr/>
+            <button onClick={change}>change</button>
+            <hr/>
+            <div>{todos[0]}</div>
+            <div>{todos[1]}</div>
+        </div>
+    );
 }
 
 export default App;
