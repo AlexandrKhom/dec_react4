97defc45c1795bfc6c72723c0d476fb3f72e0271
AlexandrKhom <alex988andr@gmail.com>
2021-04-05 22:19:24 +0300

    lesson 4.3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    firstName: ``,
+    lastName: ``,
+    email: ``,
+    age: ``,
+    pass: ``,
+  })
+
+  const handleSubmit =()=>{
+    alert(JSON.stringify(userData, null, 2))
+  }
+
+  const updateUserData = (e)=>{
+    const {target: {value, name}} = e
+    if (name === `age` && (!Number(value) || Number(value)>= 100) ){
+      return
+    }
+    setUserData({...userData, [name]: value})
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+          value={userData.firstName}
+          onChange={updateUserData}
+          type= 'text'
+          name="firstName"
+          placeholder="enter name"
+      />
+      <br/><br/>
+      <input
+          value={userData.lastName}
+          onChange={updateUserData}
+          type= 'text'
+          name="lastName"
+          placeholder="enter last"
+      />
+      <br/><br/>
+      <input
+          value={userData.email}
+          onChange={updateUserData}
+          type= 'email'
+          name="email"
+          placeholder="enter email"
+      />
+      <br/><br/>
+      <input
+          value={userData.age}
+          onChange={updateUserData}
+          type= 'number'
+          name="age"
+          placeholder="enter age"
+      />
+      <br/><br/>
+      <input
+          value={userData.pass}
+          onChange={updateUserData}
+          type= 'password'
+          name="pass"
+          placeholder="enter pass"
+      />
+      <br/><br/>
+      <button onClick={handleSubmit}>submit</button>
     </div>
   );
 }
