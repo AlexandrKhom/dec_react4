9a605efcde7bc2353d964820c08c57b7539a7012
AlexandrKhom <alex988andr@gmail.com>
2021-04-05 05:56:29 +0300

    lesson 3.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
+const url = `https://jsonplaceholder.typicode.com/todos/`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const clickFu = () => {
+        setCounter(counter + 1)
+    }
+    const [counter, setCounter] = useState(1)
+    const [todos, setTodos] = useState(null);
+    const [load, setLoad] = useState(false)
+
+    const fetchTodos = async () => {
+        setLoad(true)
+        const response = await fetch(`${url}${counter}`)
+        const data = await response.json();
+        setTimeout(() => {
+            setTodos(data);
+            setLoad(false)
+        }, 700)
+    }
+
+    useEffect(() => {
+        fetchTodos();
+        return () => {
+            setTodos(null)
+        }
+    }, [counter])
+
+
+    return (
+        <div className="App">
+            <div>
+                <button onClick={clickFu}>CLICK</button>
+                {counter}
+            </div>
+            <br/>
+            {load && (
+                <h1>LOADING.......</h1>
+            )}
+            <div>
+                {!!todos && (
+                    <h2>
+                        {todos.id} - {todos.title}
+                    </h2>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
