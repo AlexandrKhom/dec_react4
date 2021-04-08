35316f138a076a76b544b44c0cb26252e851f09f
AlexandrKhom <alex988andr@gmail.com>
2021-04-08 23:25:37 +0300

    lesson 5.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect,
-    Link
+    Link,
+    useParams, useRouteMatch, useHistory
 } from "react-router-dom";
 
 export default function App() {
@@ -17,13 +18,7 @@ export default function App() {
                             <Link to="/">Home</Link>
                         </li>
                         <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                        <li>
-                            <Link to="/test">TEST</Link>
+                            <Link to="/posts">Posts</Link>
                         </li>
                     </ul>
                 </nav>
@@ -34,22 +29,15 @@ export default function App() {
                     <Route path="/" exact>
                         <Home/>
                     </Route>
-                    <Route path="/about" render={(args) => {
-                        console.log(args);
-                        return <About/>
-                    }}/>
-
-                    <Route path="/users">
-                        {Users}
+                    <Route path="/posts" >
+                        <Posts/>
                     </Route>
-                    <Route path="/test" component={Test}>
-                    </Route>
-                    <Route>
-                        <Redirect to="/"/>
-                    </Route>
-                    {/*<Route>*/}
-                    {/*    <h1>Page not found</h1>*/}
+                    {/*<Route path="/posts/:id">*/}
+                    {/*    <PostDetails/>*/}
                     {/*</Route>*/}
+                    <Route>
+                        <h1>Page not found</h1>
+                    </Route>
                 </Switch>
             </div>
         </Router>
@@ -61,16 +49,59 @@ function Home() {
     return <h2>Home</h2>;
 }
 
-function About() {
-    return <h2>About</h2>;
+function Posts() {
+    const [posts, setPosts] = useState([])
+
+    const fetchData = async () => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const json = await resp.json();
+        setPosts(json)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return (<div>
+        <Switch>
+            <Route path="/posts/:id" exact>
+                <PostDetails/>
+            </Route>
+            <Route>
+                <Redirect to="/posts"/>
+            </Route>
+        </Switch>
+        <ul>
+            {posts.map(el => <Link to={`/posts/${el.id}`}> <li>{el.title}-{el.id}</li> </Link>)}
+        </ul>
+    </div>)
 }
 
-function Users(props) {
-    console.log(props)
-    return <h2>Users</h2>;
-}
+    function PostDetails () {
+        const [post, setPost] = useState();
 
-function Test(props) {
-    console.log(props)
-    return <h2>Test Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, provident.</h2>
-}
+        // const match = useRouteMatch();
+        const {id} = useParams();
+        const history = useHistory()
+
+
+
+        const fetchData = async () => {
+            const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+            const json = await resp.json();
+            setPost(json)
+        }
+
+        useEffect(() => {
+            fetchData()
+        }, [id])
+
+        return (<div>
+            <h2>post details</h2>
+                {post && <h1>{post.title}-{post.body}</h1>}
+<br/>
+<button onClick={()=> history.push(`/posts/${+id+1}`)}>go to next</button>
+        </div>)
+    }
+
+
