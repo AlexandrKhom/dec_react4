5e6f40f23cc9dded5c096c7ef7abb2adfc128536
AlexandrKhom <alex988andr@gmail.com>
2021-04-08 18:33:31 +0300

    lesson 5.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+    Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                        <li>
+                            <Link to="/test">TEST</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/" exact>
+                        <Home/>
+                    </Route>
+                    <Route path="/about" render={(args) => {
+                        console.log(args);
+                        return <About/>
+                    }}/>
+
+                    <Route path="/users">
+                        {Users}
+                    </Route>
+                    <Route path="/test" component={Test}>
+                    </Route>
+                    <Route>
+                        <Redirect to="/"/>
+                    </Route>
+                    {/*<Route>*/}
+                    {/*    <h1>Page not found</h1>*/}
+                    {/*</Route>*/}
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function Home() {
+
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users(props) {
+    console.log(props)
+    return <h2>Users</h2>;
+}
+
+function Test(props) {
+    console.log(props)
+    return <h2>Test Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, provident.</h2>
+}
