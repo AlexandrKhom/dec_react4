db64a55daa621e66daa353f0bf47394521c2225f
AlexandrKhom <alex988andr@gmail.com>
2021-04-09 22:31:38 +0300

    lesson 6.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, {useState, createContext, useContext} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+const CounterContext = createContext();
+
+const ContextProvider = ({children}) => {
+    const [counter, setCounter] = useState(0)
+
+    const incCounter = ()=> {
+        setCounter(counter+1)
+    }
+    const decCounter = ()=> {
+        setCounter(counter-1)
+    }
+    return (
+        <CounterContext.Provider value={{counter, incCounter, decCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
+
+const Counter = () => {
+    const {counter, incCounter, decCounter} = useContext(CounterContext)
+
+    return (
+        <div>
+            <h2>Counter: {counter}</h2>
+            <br/>
+            <button onClick={incCounter}>inc</button>
+            <button onClick={decCounter}>dec</button>
+        </div>
+    )
+}
+
+const Header = () => {
+    const {counter} = useContext(CounterContext)
+
+    return (
+        <h2>Header  counter: {counter}</h2>
+    )
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <ContextProvider>
+            <Counter/>
+            <br/>
+            <br/>
+            <Header/>
+            </ContextProvider>
+        </div>
+    );
 }
 
 export default App;
